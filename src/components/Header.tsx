6370bf8674c1a7740b9874bd9be3b722002d62a8
Austin Wilson <wilson.austin10@gmail.com>
2025-05-20 18:54:57 -0500

    fixed file structure

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -9,7 +9,7 @@ export default function Header() {
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-4">
         <div className="flex items-center justify-between">
           <Link href="/" className="flex items-center">
-            <div className="relative w-[320px] h-[85px]">
+            <div className="relative w-[400px] h-[100px]">
               <Image
                 src="/NexStep.png"
                 alt="NexStep HomeBuyers LLC"
