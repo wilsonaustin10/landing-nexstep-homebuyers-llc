a5e834a2a63238f6582c2e25768ad5ab42632d2f
Austin Wilson <wilson.austin10@gmail.com>
2025-09-19 10:12:55 -0500

    Add Google Tag Manager integration with container ID GTM-P5VS4LQX

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import './globals.css';
 import OptimizedClientLayout from '../components/OptimizedClientLayout';
 import Script from 'next/script';
 import CriticalCSS from '../components/CriticalCSS';
+import GoogleAnalytics from '../components/GoogleAnalytics';
 
 const inter = Inter({ 
   subsets: ['latin'],
@@ -22,11 +23,37 @@ export default function RootLayout({
         {/* Google Site Verification - Replace with your actual verification code */}
         <meta name="google-site-verification" content="google-site-verification-code" />
         <CriticalCSS />
+        {/* Google Tag Manager */}
+        <Script
+          id="gtm-script"
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `
+              (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+              new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+              j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+              'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+              })(window,document,'script','dataLayer','GTM-P5VS4LQX');
+            `,
+          }}
+        />
+        {/* End Google Tag Manager */}
       </head>
       <body className={inter.className}>
+        {/* Google Tag Manager (noscript) */}
+        <noscript>
+          <iframe
+            src="https://www.googletagmanager.com/ns.html?id=GTM-P5VS4LQX"
+            height="0"
+            width="0"
+            style={{ display: 'none', visibility: 'hidden' }}
+          />
+        </noscript>
+        {/* End Google Tag Manager (noscript) */}
         <OptimizedClientLayout>
           {children}
         </OptimizedClientLayout>
+        <GoogleAnalytics />
       </body>
     </html>
   );
